1a5b493e6eb19b843c042185e0a2537064da7fa7
SweepHee <jeonsh1991@gmail.com>
2023-11-15 14:00:07 +0900

    clean

--- common/src/main/java/sc2/syrup/common/util/random.kt
+++
@@ -1,8 +0,0 @@
-package sc2.syrup.common.util
-
-fun generateRandomString(length: Int = 16): String {
-    val allowedChars = ('A'..'Z') + ('a'..'z') + ('0'..'9')
-    return (1..length)
-        .map { allowedChars.random() }
-        .joinToString("")
-}
